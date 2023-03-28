374758e7ef4a478059aef16843b8756a0f251c3f
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-03-28 11:16:34 +0200

    routes, login and register functionalities

--- server/src/index.js
+++ server/src/index.js
@@ -3,11 +3,13 @@ const app=express();
 const {PORT}=require('./constants/index');
 const authRouter=require('./routes/auth');
 const cors=require('cors');
+const cookieParser=require('cookie-parser');
 
 app.use(express.json());
 app.use(cors({
    origin:'http:localhost:8000'
 }));
+app.use(cookieParser());
 app.use('/api/v1',authRouter);
 
 const appStart=()=>{
